ecb777da7f982a38d30b944c5343b3be114e415f
elchudi <gustavogiudici@gmail.com>
2015-04-16 16:17:01 -0300

    fixed ansible static inventory creation

--- terraform/tf_to_inventory.py
+++ terraform/tf_to_inventory.py
@@ -1,33 +1,57 @@
 #!/usr/bin/env python
 import json
 
-hosts = {}
+associated_roles = {
+    "dev":["node"], 
+    "vim":["node"], 
+    "webdev":["consulClient"], 
+    "connectorrest":["consulClient", "node", "zeromq"], 
+    "chaski":["consulClient", "node", "zeromq"], 
+    "atahualpa":["consulClient", "node", "zeromq"]
+}
+
 groups = {}
+ssh_keys_path = "./sshkeys/"
+ssh_key_extension = ".key"
 
 with open("terraform.tfstate") as fp:
     state = json.load(fp)
 
-for module in state["modules"]:
-    for key, resource in module["resources"].iteritems():
-        print key.split(".")
-        resource_type, group_name = key.split(".")
-        
-        print resource
-        #index = int(index)
-        attributes = resource["primary"]["attributes"]
-        print attributes
-        for key2, res in attributes.iteritems():
-            print key2, res
-        name = attributes["name"]
+with open("hosts", "w") as inventory:
+    for role, extra_roles in associated_roles.iteritems():
+        for extra_role in extra_roles:
+            groups[extra_role] = []
+    for module in state["modules"]:
+        for key, resource in module["resources"].iteritems():
+            attributes = resource["primary"]["attributes"]
+            groups[attributes["metadata.type"]] = []
+    for module in state["modules"]:
+        for key, resource in module["resources"].iteritems():
+            #print key.split(".")
+            resource_type, group_name = key.split(".")
+            
+            #print resource
+            #index = int(index)
+            attributes = resource["primary"]["attributes"]
+            # print attributes
+            for key2, res in attributes.iteritems():
+                print key2, res
+            groups[attributes["metadata.type"]].append(attributes["name"])
+            if attributes["metadata.type"] in associated_roles.keys():
+                for extra_role in associated_roles[attributes["metadata.type"]]:
+                    groups[extra_role].append(attributes["name"])
+                
+    
+           
 
-        if attributes["status"] == "active":
-            hosts[name] = attributes["ipv4_address"]
-            #groups.setdefault(group_name, {})[name] = { "index": index, "nz_index": index + 1, }
+            inventory.write("{} ansible_ssh_host={} ansible_ssh_user={} ansible_ssh_private_key_file={}{}{}\n".format(attributes["name"], attributes["access_ip_v4"], "root", ssh_keys_path, attributes["key_pair"], ssh_key_extension))
 
-with open("hosts", "w") as fp:
-    for host, ip_address in hosts.iteritems():
-        fp.write("{} ansible_ssh_host={} ansible_ssh_user=root\n".format(host, ip_address))
-
+    #apply 
+    for role, names in groups.iteritems():
+        inventory.write("[{}]\n".format(role))
+        for name in names:
+            inventory.write("{}\n".format(name))
+"""
     for group, hosts in groups.iteritems():
         fp.write("[{}]\n".format(group))
         for host, variables in hosts.iteritems():
@@ -35,3 +59,4 @@ with open("hosts", "w") as fp:
             for k, v in variables.iteritems():
                 fp.write(" {}={}".format(k, v))
             fp.write("\n")
+"""
