466ea5a1690ae77093aeb96a23ab25229dfb9cb5
elchudi <gustavogiudici@gmail.com>
2015-04-27 19:46:00 -0300

    changed bak to previous name

--- createAnsibleStaticInventory.py
+++ createAnsibleStaticInventory.py
@@ -1,7 +1,7 @@
 from jinja2 import Template
 import yaml
 
-templateVirtualBox = Template(open('templates/ansible_static_inventory_virtualbox.j2').read())
+templateVirtualBox = Template(open('templates/ansible_static_inventory.j2').read())
 datavars = yaml.load(open("vagrantConfig.yml").read())
 
 output =  templateVirtualBox.render(datavars)
